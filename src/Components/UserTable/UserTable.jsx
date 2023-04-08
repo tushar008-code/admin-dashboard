a6d33e5e24467092174f566d527312a66d200226
Tushar Dutta <td0540245@gmail.com>
2023-04-08 21:27:19 +0530

    list section started

--- src/Components/UserTable/UserTable.jsx
+++ src/Components/UserTable/UserTable.jsx
@@ -11,48 +11,99 @@ function UserTable() {
     return { name, calories, fat, carbs, protein };
   }
 
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-  ];
-  return (
-    <>
-      <div className="tables-user">
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right">{row.carbs}</TableCell>
-                  <TableCell align="right">{row.protein}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-    </>
-  );
+ const rows = [
+   {
+     id: 1143155,
+     product: "Acer Nitro 5",
+     img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+     customer: "John Smith",
+     date: "1 March",
+     amount: 785,
+     method: "Cash on Delivery",
+     status: "Approved",
+   },
+   {
+     id: 2235235,
+     product: "Playstation 5",
+     img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
+     customer: "Michael Doe",
+     date: "1 March",
+     amount: 900,
+     method: "Online Payment",
+     status: "Pending",
+   },
+   {
+     id: 2342353,
+     product: "Redragon S101",
+     img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
+     customer: "John Smith",
+     date: "1 March",
+     amount: 35,
+     method: "Cash on Delivery",
+     status: "Pending",
+   },
+   {
+     id: 2357741,
+     product: "Razer Blade 15",
+     img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
+     customer: "Jane Smith",
+     date: "1 March",
+     amount: 920,
+     method: "Online",
+     status: "Approved",
+   },
+   {
+     id: 2342355,
+     product: "ASUS ROG Strix",
+     img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
+     customer: "Harold Carol",
+     date: "1 March",
+     amount: 2000,
+     method: "Online",
+     status: "Pending",
+   },
+ ];
+ return (
+   <>
+     <div className="tables-user">
+       <TableContainer component={Paper} className="table">
+         <Table sx={{ minWidth: 650 }} aria-label="simple table">
+           <TableHead>
+             <TableRow>
+               <TableCell className="tableCell">Tracking ID</TableCell>
+               <TableCell className="tableCell">Product</TableCell>
+               <TableCell className="tableCell">Customer</TableCell>
+               <TableCell className="tableCell">Date</TableCell>
+               <TableCell className="tableCell">Amount</TableCell>
+               <TableCell className="tableCell">Payment Method</TableCell>
+               <TableCell className="tableCell">Status</TableCell>
+             </TableRow>
+           </TableHead>
+           <TableBody>
+             {rows.map((row) => (
+               <TableRow key={row.id}>
+                 <TableCell className="tableCell">{row.id}</TableCell>
+                 <TableCell className="tableCell">
+                   <div className="cellWrapper">
+                     <img src={row.img} alt="" className="image" />
+                     {row.product}
+                   </div>
+                 </TableCell>
+                 <TableCell className="tableCell">{row.customer}</TableCell>
+                 <TableCell className="tableCell">{row.date}</TableCell>
+                 <TableCell className="tableCell">{row.amount}</TableCell>
+                 <TableCell className="tableCell">{row.method}</TableCell>
+                 <TableCell className="tableCell">
+                   <span className={`status ${row.status}`}>{row.status}</span>
+                 </TableCell>
+               </TableRow>
+             ))}
+           </TableBody>
+         </Table>
+       </TableContainer>
+     </div>
+   </>
+ );
 }
 
 export default UserTable;
--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-function List() {
-  return <div>List</div>;
+import MainNavbar from "../../Components/Mainbar/MainNavbar";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import DataTable from "../../Components/DataTable/DataTable";
+
+function Home() {
+  return (
+    <>
+      <main className="grid-2">
+        <Sidebar />
+        <div className="container">
+          <MainNavbar />
+          <DataTable />
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default List;
+export default Home;
